d084a58d62695e71950ba83038908f0c840afb52
Guga15gu <guga15gu@users.noreply.github.com>
2026-07-16 21:10:24 +0000

    add createMuscle and createExercise

---
+++ src/data/exercise.js
@@ -0,0 +1,27 @@
+function createMuscle(name){
+  if (typeof name !== "string"){
+    throw new Error("Name is not string");
+  }
+
+  return {id: crypto.randomUUID(), name}
+}
+
+function createExercise(name, muscles){
+  if (typeof name !== "string"){
+    throw new Error("Name is not string");
+  }
+  if (!Array.isArray(muscles)) {
+    throw new Error("Muscles is not array");
+  }
+  if (muscles.length === 0) {
+    throw new Error("Muscles is an empty array");
+  }
+
+  for(const muscle of muscles){
+    if (typeof muscle !== "string"){
+      throw new Error(`Muscle id ${muscle} is not a string`);
+    }
+  }
+
+  return {id: crypto.randomUUID(), name, muscles}
+}
