9f3e83946ebe7752202f6f243686108a209bd7ad
Guga15gu <guga15gu@users.noreply.github.com>
2026-07-17 18:32:41 +0000

    feat: add exercise store with localStorage persistance

---
+++ src/data/exerciseStore.js
@@ -0,0 +1,16 @@
+export { getExercises, addExercise }
+
+function getExercises(){
+    const exercises = localStorage.getItem("exercises");
+    if (exercises === null){
+        return {};
+    }
+
+    return JSON.parse(exercises);
+}
+function addExercise(exercise){
+    const exercises = getExercises();
+    exercises[exercise.id] = exercise;
+
+    localStorage.setItem("exercises", JSON.stringify(exercises));
+}
