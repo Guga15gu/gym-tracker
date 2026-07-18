a5233a02b2616f86c9545dc9b2e27ef4d8a272aa
Guga15gu <guga15gu@users.noreply.github.com>
2026-07-18 17:50:09 +0000

    feat: add muscle list component

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ function App() {
 
   return (
     <>
+      <MuscleList></MuscleList>
       <section id="center">
         <div className="hero">
           <img src={heroImg} className="base" width="170" height="179" alt="" />
