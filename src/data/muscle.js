c94fe9ddb3ea2a57728dc7852103163e4d6c685c
Guga15gu <guga15gu@users.noreply.github.com>
2026-07-19 13:27:41 +0000

    feat: validate if muscle name is not empty

--- src/data/muscle.js
+++ src/data/muscle.js
@@ -2,9 +2,11 @@ export function createMuscle(name) {
   if (typeof name !== "string") {
     throw new Error("Name is not string");
   }
-  if (name === "") {
+  const trimmedName = name.trim();
+
+  if (trimmedName === "") {
     throw new Error("Name is empty");
   }
 
-  return { id: crypto.randomUUID(), name };
+  return { id: crypto.randomUUID(), name: trimmedName };
 }
