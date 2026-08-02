422d7aa104be2bfc733a00d407403c80f54c40e2
Guga15gu <guga15gu@users.noreply.github.com>
2026-08-02 19:18:06 +0000

    refactor: migrate exercise module to TypeScript

--- src/data/exercise.ts
+++ src/data/exercise.ts
@@ -1,21 +1,11 @@
-export { createExercise };
-
+export type Exercise = { id: string; name: string; muscles: string[] };
 /**
- * @typedef {Object} Exercise
- * @property {string} id - UUID
- * @property {string} name
- * @property {string[]} muscles - array of muscle ids
- */
-/**
- * @param {string} name
- * @param {string[]} muscles - array of muscle ids
- * @returns {Exercise}
  * @throws {Error} if name is not string
  * @throws {Error} if muscles is not array
  * @throws {Error} if muscles is empty array
  * @throws {Error} if an element of muscle array is not a string
  */
-function createExercise(name, muscles) {
+export function createExercise(name: string, muscles: string[]): Exercise {
   if (typeof name !== "string") {
     throw new Error("Name is not string");
   }
