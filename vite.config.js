3ee2f56d9b34b58a1bc7e32d2552bc111493ac6d
Guga15gu <guga15gu@users.noreply.github.com>
2026-08-02 17:37:52 +0000

    feat: installable and runs offline on mobile

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,24 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      manifest: {
+        name: "gus-gym-tracker",
+        short_name: "gym-tracker",
+        display: "standalone",
+        theme_color: "#ffffff",
+        background_color: "#ffffff",
+        icons: [
+          { src: "/pwa-192.png", sizes: "192x192", type: "image/png" },
+          { src: "/pwa-512.png", sizes: "512x512", type: "image/png" },
+        ],
+      },
+    }),
+  ],
   server: { host: true },
 });
